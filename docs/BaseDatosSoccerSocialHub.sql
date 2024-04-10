af268ce5496fac5ef861ee323992923b491b3ad8
Abelgs716 <117741468+Abelgs716@users.noreply.github.com>
2024-04-10 23:19:35 +0200

    add arreglos modelo de datos

--- docs/BaseDatosSoccerSocialHub.sql
+++ docs/BaseDatosSoccerSocialHub.sql
@@ -8,24 +8,24 @@ CREATE TABLE users (
 );
 
 CREATE TABLE user_rol (
-    usuario_id INT PRIMARY KEY,
+    user_id INT PRIMARY KEY,
     roles_usuario ENUM('ROLE_USER', 'ROLE_ADMIN') NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE teams (
     teams_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_created INT,
     name VARCHAR(100) NOT NULL,
-    size INT NOT NULL
+    size INT NOT NULL,
+    FOREIGN KEY (user_created) REFERENCES users(id)
 );
 
 CREATE TABLE players (
     players_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
     name VARCHAR(100) NOT NULL,
     age INT NOT NULL,
     team_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (team_id) REFERENCES teams(teams_id)
 );
 
@@ -42,7 +42,19 @@ CREATE TABLE reviews (
     reviews_id INT PRIMARY KEY AUTO_INCREMENT,
     description VARCHAR(100) NOT NULL,
     fields_id INT,
-    team_id INT,
+    players_id INT,
     FOREIGN KEY (fields_id) REFERENCES fields(fields_id),
-    FOREIGN KEY (team_id) REFERENCES teams(teams_id)
+    FOREIGN KEY (players_id) REFERENCES players(players_id)
+);
+
+CREATE TABLE matches  (
+    id_partido INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    team1 INT,
+    team2 INT,
+    fields_id INT,
+    FOREIGN KEY (team1) REFERENCES teams(teams_id),
+    FOREIGN KEY (team2) REFERENCES teams(teams_id),
+    FOREIGN KEY (fields_id) REFERENCES fields(fields_id)
 );
