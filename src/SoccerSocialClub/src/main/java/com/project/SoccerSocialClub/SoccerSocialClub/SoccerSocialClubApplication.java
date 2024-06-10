e97182276132995bf61c8be501e3372d66caa22c
“abelgs716” <abelgs716@gmail.com>
2024-06-11 00:46:09 +0200

    config Despliegue

--- src/SoccerSocialClub/src/main/java/com/project/SoccerSocialClub/SoccerSocialClub/SoccerSocialClubApplication.java
+++ src/SoccerSocialClub/src/main/java/com/project/SoccerSocialClub/SoccerSocialClub/SoccerSocialClubApplication.java
@@ -1,13 +1,51 @@
 package com.project.SoccerSocialClub.SoccerSocialClub;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class SoccerSocialClubApplication {
 
 	public static void main(String[] args) {
+		if (System.getenv("AWS_EXECUTION_ENV") == null) {
+			int maxDepth = 5;
+			Optional<Path> envPathOptional = findEnvFile(Paths.get("").toAbsolutePath(), maxDepth);
+			if (envPathOptional.isPresent()) {
+				Path envPath = envPathOptional.get();
+				Dotenv dotenv = Dotenv.configure().directory(envPath.getParent().toString()).load();
+				dotenv.entries().forEach(entry -> {
+					System.setProperty(entry.getKey(), entry.getValue());
+				});
+			} else {
+				System.out.println(".env file not found!");
+			}
+		}
+
 		SpringApplication.run(SoccerSocialClubApplication.class, args);
 	}
 
+	private static Optional<Path> findEnvFile(Path startPath, int maxDepth) {
+		Path currentPath = startPath;
+		int depth = 0;
+		while (currentPath != null && depth <= maxDepth) {
+			Path envFile = currentPath.resolve(".env");
+			if (Files.exists(envFile)) {
+				return Optional.of(envFile);
+			}
+			currentPath = currentPath.getParent();
+			depth++;
+		}
+		return Optional.empty();
+	}
 }
--- src/SoccerSocialClubFront/src/environments/environment.prod.ts
+++ src/SoccerSocialClubFront/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://soccersocialclub.us-east-1.elasticbeanstalk.com/api'
 };
