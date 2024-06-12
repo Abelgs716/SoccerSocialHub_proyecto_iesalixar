8b394e1a3d6db109c9fe8e6098eca4e4904e067e
“abelgs716” <abelgs716@gmail.com>
2024-06-13 00:17:53 +0200

    add change sql file

--- docs/BaseDatosSoccerSocialHub.sql
+++ docs/BaseDatosSoccerSocialHub.sql
@@ -1,60 +1,120 @@
-CREATE DATABASE IF NOT EXISTS SoccerSocialHub;
-USE SoccerSocialHub;
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(100) NOT NULL
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE user_rol (
-    user_id INT PRIMARY KEY,
-    roles_usuario ENUM('ROLE_USER', 'ROLE_ADMIN') NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema soccersocial
+-- -----------------------------------------------------
 
-CREATE TABLE teams (
-    teams_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_created INT,
-    name VARCHAR(100) NOT NULL,
-    size INT NOT NULL,
-    FOREIGN KEY (user_created) REFERENCES users(id)
-);
+-- -----------------------------------------------------
+-- Schema soccersocial
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `soccersocial` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `soccersocial` ;
 
-CREATE TABLE players (
-    players_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    age INT NOT NULL,
-    team_id INT,
-    FOREIGN KEY (team_id) REFERENCES teams(teams_id)
-);
+-- -----------------------------------------------------
+-- Table `soccersocial`.`evento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `soccersocial`.`evento` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `descripcion` VARCHAR(255) NOT NULL,
+  `descripcion_larga` VARCHAR(255) NOT NULL,
+  `estado` INT NOT NULL,
+  `fecha_inicio_evento` DATETIME(6) NOT NULL,
+  `id_creador` BIGINT NOT NULL,
+  `imagen` TEXT NULL DEFAULT NULL,
+  `max_personas` INT NOT NULL,
+  `nombre_evento` VARCHAR(255) NOT NULL,
+  `ubicacion` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE fields (
-    fields_id INT PRIMARY KEY AUTO_INCREMENT,
-    location VARCHAR(100) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    capacity INT,
-    team_id INT,
-    FOREIGN KEY (team_id) REFERENCES teams(teams_id)
-);
 
-CREATE TABLE reviews (
-    reviews_id INT PRIMARY KEY AUTO_INCREMENT,
-    description VARCHAR(100) NOT NULL,
-    fields_id INT,
-    users_id INT,
-    FOREIGN KEY (fields_id) REFERENCES fields(fields_id),
-    FOREIGN KEY (users_id) REFERENCES users(id)
-);
+-- -----------------------------------------------------
+-- Table `soccersocial`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `soccersocial`.`usuario` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `estado` INT NOT NULL,
+  `nombre` VARCHAR(255) NOT NULL,
+  `nombre_usuario` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `role` TINYINT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `UK5171l57faosmj8myawaucatdw` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `UKpuhr3k3l7bj71hb7hk7ktpxn0` (`nombre_usuario` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE matches  (
-    matches_id INT PRIMARY KEY AUTO_INCREMENT,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    team1 INT,
-    team2 INT,
-    fields_id INT,
-    FOREIGN KEY (team1) REFERENCES teams(teams_id),
-    FOREIGN KEY (team2) REFERENCES teams(teams_id),
-    FOREIGN KEY (fields_id) REFERENCES fields(fields_id)
-);
+
+-- -----------------------------------------------------
+-- Table `soccersocial`.`evento_organizadores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `soccersocial`.`evento_organizadores` (
+  `evento_id` BIGINT NOT NULL,
+  `organizador_id` BIGINT NOT NULL,
+  INDEX `FKp2n1y59mcxpyrhqno6g8kdwmj` (`organizador_id` ASC) VISIBLE,
+  INDEX `FKmdntmoj7380u4gw9epbge399l` (`evento_id` ASC) VISIBLE,
+  CONSTRAINT `FKmdntmoj7380u4gw9epbge399l`
+    FOREIGN KEY (`evento_id`)
+    REFERENCES `soccersocial`.`evento` (`id`),
+  CONSTRAINT `FKp2n1y59mcxpyrhqno6g8kdwmj`
+    FOREIGN KEY (`organizador_id`)
+    REFERENCES `soccersocial`.`usuario` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `soccersocial`.`evento_usuarios_inscritos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `soccersocial`.`evento_usuarios_inscritos` (
+  `evento_id` BIGINT NOT NULL,
+  `usuarios_inscritos_id` BIGINT NOT NULL,
+  INDEX `FKfogw1r08q8kcppqx69tf88wfh` (`usuarios_inscritos_id` ASC) VISIBLE,
+  INDEX `FKfsmd8fwbm5d7j67dafws7jyww` (`evento_id` ASC) VISIBLE,
+  CONSTRAINT `FKfogw1r08q8kcppqx69tf88wfh`
+    FOREIGN KEY (`usuarios_inscritos_id`)
+    REFERENCES `soccersocial`.`usuario` (`id`),
+  CONSTRAINT `FKfsmd8fwbm5d7j67dafws7jyww`
+    FOREIGN KEY (`evento_id`)
+    REFERENCES `soccersocial`.`evento` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `soccersocial`.`usuario_eventos_inscritos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `soccersocial`.`usuario_eventos_inscritos` (
+  `usuario_id` BIGINT NOT NULL,
+  `eventos_inscritos_id` BIGINT NOT NULL,
+  INDEX `FKbqnqu3mnxu39ikv9ee9j6m419` (`eventos_inscritos_id` ASC) VISIBLE,
+  INDEX `FKihr9no8dv5dogldm4hq32smrs` (`usuario_id` ASC) VISIBLE,
+  CONSTRAINT `FKbqnqu3mnxu39ikv9ee9j6m419`
+    FOREIGN KEY (`eventos_inscritos_id`)
+    REFERENCES `soccersocial`.`evento` (`id`),
+  CONSTRAINT `FKihr9no8dv5dogldm4hq32smrs`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `soccersocial`.`usuario` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
